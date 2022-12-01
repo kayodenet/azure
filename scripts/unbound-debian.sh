68f7c1fe3bb31f11706d9c3cd51b7daa91205c62
Kayode Salawu <salawu@gmail.com>
2022-12-01 12:27:09 +0000

    added probe script to dns servers

--- scripts/unbound-debian.sh
+++ scripts/unbound-debian.sh
@@ -50,3 +50,60 @@ forward-zone:
 EOF
 
 /etc/init.d/unbound restart
+systemctl restart unbound
+systemctl enable unbound
+
+# test scripts
+#-----------------------------------
+
+# ping-ip
+
+cat <<EOF > /usr/local/bin/ping-ip
+echo -e "\n ping ip ...\n"
+%{ for target in values(TARGETS) ~}
+echo ${target} - \$(ping -qc2 -W1 ${target} 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')
+%{ endfor ~}
+EOF
+chmod a+x /usr/local/bin/ping-ip
+
+# ping-dns
+
+cat <<EOF > /usr/local/bin/ping-dns
+echo -e "\n ping dns ...\n"
+%{ for target in keys(TARGETS) ~}
+echo ${target} - \$(ping -qc2 -W1 ${target} 2>&1 | awk -F'/' 'END{ print (/^rtt/? "OK "\$5" ms":"NA") }')
+%{ endfor ~}
+EOF
+chmod a+x /usr/local/bin/ping-dns
+
+# curl-ip
+
+cat <<EOF > /usr/local/bin/curl-ip
+echo -e "\n curl ip ...\n"
+%{ for target in values(TARGETS) ~}
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%%{http_code} (%%{time_total}s) - %%{remote_ip}" -s -o /dev/null ${target}) - ${target}"
+%{ endfor ~}
+EOF
+chmod a+x /usr/local/bin/curl-ip
+
+# curl-dns
+
+cat <<EOF > /usr/local/bin/curl-dns
+echo -e "\n curl dns ...\n"
+%{ for target in keys(TARGETS) ~}
+echo  "\$(curl -kL --max-time 2.0 -H 'Cache-Control: no-cache' -w "%%{http_code} (%%{time_total}s) - %%{remote_ip}" -s -o /dev/null ${target}) - ${target}"
+%{ endfor ~}
+EOF
+chmod a+x /usr/local/bin/curl-dns
+
+# trace-ip
+
+cat <<EOF > /usr/local/bin/trace-ip
+echo -e "\n trace ip ...\n"
+%{ for target in values(TARGETS) ~}
+traceroute ${target}
+echo ""
+%{ endfor ~}
+EOF
+chmod a+x /usr/local/bin/trace-ip
+
